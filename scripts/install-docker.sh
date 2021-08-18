36a0ebc1e3f6d0b1643afe8db63dbed1096a6259
Nguyen Anh Phuong <30553637+N-A-P@users.noreply.github.com>
2021-08-18 14:02:25 +0700

    edit permission

--- scripts/install-docker.sh
+++ scripts/install-docker.sh
@@ -10,7 +10,7 @@ systemctl enable docker.service
 echo "Cai dat docker compose"
 sleep 3
 sudo curl -L https://github.com/docker/compose/releases/download/1.18.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
-sudo chmod +x /usr/local/bin/docker-compose
+sudo chmod 774 /usr/local/bin/docker-compose
 
 sleep 3
 echo "Kiem tra phien ban docker va docker compose"
